a02ee8e2a3397f4b5e06352ec7600faf941701e1
jengahmedeyan <jengahmedeyan@gmail.com>
2022-06-09 23:39:38 +0000

    project price condition formatted

--- src/com/company/Product.java
+++ src/com/company/Product.java
@@ -1,21 +1,20 @@
 package com.company;
 
 public class Product {
-  private String productName;
   private int productPrice;
+  private String productName;
   private String productCategory;
-
 
   public Product(String productName, String productCategory, int productPrice) {
     this.productName = productName;
-//    this.productPrice = productPrice;
+    // this.productPrice = productPrice;
     this.productCategory = productCategory;
-    if(productPrice < 0){
+    
+    if (productPrice < 0) {
       throw new IllegalArgumentException("Product price cannot be less than 0");
     }
 
     this.productPrice = productPrice;
-
 
   }
 
